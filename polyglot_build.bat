9f337d1c59c051d6c46b095c815c68b63f4f2c68
Joe <mlfaw@protonmail.com>
2018-11-13 23:18:19 +0000

    polyglot_build.bat - linux builds in build_linux and win32 builds in build_win32

--- polyglot_build.bat
+++ polyglot_build.bat
@@ -3,20 +3,17 @@ echo off
 echo ; set +v # > NUL
 echo ; function GOTO { true; } # > NUL
 
-mkdir build
-mkdir build/libressl
-
 GOTO WIN
 # btw this is from https://gist.github.com/prail/24acc95908e581722c0e9df5795180f6
 
-cmake -Bbuild/libressl -H"thirdparty/libressl-2.8.2" -DCMAKE_BUILD_TYPE=Debug -DLIBRESSL_SKIP_INSTALL=ON -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF
-cmake --build build/libressl/ -- -j$(nproc)
-cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug
-cmake --build build/ -- -j$(nproc)
+cmake -Bbuild_linux/libressl -H"thirdparty/libressl-2.8.2" -DCMAKE_BUILD_TYPE=Debug -DLIBRESSL_SKIP_INSTALL=ON -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF
+cmake --build build_linux/libressl/ -- -j$(nproc)
+cmake -Bbuild_linux -H. -DCMAKE_BUILD_TYPE=Debug
+cmake --build build_linux/ -- -j$(nproc)
 exit 0
 
 :WIN
-cmake -Bbuild/libressl -H"thirdparty/libressl-2.8.2" -DCMAKE_BUILD_TYPE=Debug -DLIBRESSL_SKIP_INSTALL=ON -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF -G"Visual Studio 15 2017 Win64"
-cmake --build build/libressl/ --config Debug
-cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug -G"Visual Studio 15 2017 Win64"
-cmake --build build/ --config Debug
+cmake -Bbuild_win32/libressl -H"thirdparty/libressl-2.8.2" -DCMAKE_BUILD_TYPE=Debug -DLIBRESSL_SKIP_INSTALL=ON -DLIBRESSL_APPS=OFF -DLIBRESSL_TESTS=OFF -G"Visual Studio 15 2017 Win64"
+cmake --build build_win32/libressl/ --config Debug
+cmake -Bbuild_win32 -H. -DCMAKE_BUILD_TYPE=Debug -G"Visual Studio 15 2017 Win64"
+cmake --build build_win32/ --config Debug
