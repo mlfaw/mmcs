a7a245dfa263b095373a69f74fefd1c4e48aca4a
Joe <mlfaw@protonmail.com>
2018-12-16 22:52:12 +0000

    Remove mmcs_StringHelpers and use code in mmcs_NativeMessaging

--- src/mmcs_NativeMessaging.cpp
+++ src/mmcs_NativeMessaging.cpp
@@ -1,6 +1,6 @@
 /*{REPLACEMEWITHLICENSE}*/
 #include "mmcs_NativeMessaging.hpp"
-#include "mmcs_StringHelpers.hpp"
+#include <string.h> // strrchr(), strcmp()
 
 namespace mmcs {
 namespace NativeMessaging {
@@ -21,26 +21,30 @@ static bool IsMode(int argc, oschar ** argv)
 	// Chrome Windows:
 	//   mmcs chrome-extension://[extensionID] --parent-window=1234
 
+	const oschar * extension_id;
+	const oschar * manifest_ext;
+
 	if (argc < 2)
 		return false;
 
-	// Check for Chrome...
-	if (osStringInArray(argv[1], allowedExtensions, allowedExtensionsCount))
-		return true;
+	// Chrome's extension_id location.
+	extension_id = argv[1];
 
-	if (argc < 3)
-		return false;
+	// Check for the Firefox extension manifest.json file...
+	manifest_ext = osstrrchr(argv[1], _OS('.'));
+	if (manifest_ext && (osstrcmp(manifest_ext, _OS(".json")) == 0)) {
+		// Yes, Firefox triggered us so the extension_id is argv[2].
+		if (argc < 3)
+			return false;
+		extension_id = argv[2];
+	}
 
-	// Now for Firefox...
-	// The FF manifest...
-	if (!osStringEndsWithExtension(argv[1], _OS(".json")))
-		return false;
+	for (int i = 0; i < allowedExtensionsCount; ++i) {
+		if (osstrcmp(allowedExtensions[i], extension_id) == 0)
+			return true;
+	}
 
-	// The FF extension...
-	if (!osStringInArray(argv[2], allowedExtensions, allowedExtensionsCount))
-		return false;
-
-	return true;
+	return false;
 }
 
 bool Handler(int argc, oschar ** argv)
